140f1d5b810c17b7f8a5e7efacf852101711c06a
Felipe Martins <felipedesmartins@gmail.com>
2023-10-29 21:05:28 -0300

    Update root components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React, { ReactNode }  from 'react';
 import './App.css';
+import AuthProvider from './provider/authProvider';
 
-function App(props:{children:ReactNode}) {
+import Routes from './routes';
+
+function App() {
 
   return (
     <div className="App">
-      {props.children}
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </div>
   );
 }
